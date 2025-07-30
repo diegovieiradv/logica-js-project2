de34a04d72441589865cb5742c9674fb48387da7
Diego Vieira <dvdiego87@gmail.com>
2025-07-30 06:07:49 -0300

    adicionei os elementos do project

---
+++ app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Jogo do número secreto';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolha um número entre 1 e 10 e tente adivinhar o número secreto!';
+
+function verificarChute(){
+    console.log('chutei')
+}
